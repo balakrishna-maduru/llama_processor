592a336ada02bca2cc2326a33550de3799cd9906
balakrishna-maduru <balakrishnamaduru@gmail.com>
2023-11-26 15:52:31 +0800

    rename tests folder

---
+++ resources/data/mongo_db_sqls/example_1.sql
@@ -0,0 +1,11 @@
+db.getCollection('docstore/data').find({
+  "__data__.metadata.page_label": "1",
+  "__data__.metadata.file_name": "lyft_2021.pdf",
+  "__data__.hash":"4933bb9181afa7862a800bb96428687e71875ca7a4006e557e441a3d60bd7e2d"
+})
+
+db.getCollection('docstore/data').find({
+  "__data__.metadata.page_label": "1",
+  "__data__.metadata.file_name": "lyft_2021.pdf",
+  "__data__.hash":"c74f06eb59a0abe6305a87a06b2f83bc82a6771f0bc20a3d9a907dca00b2dce5"
+})
